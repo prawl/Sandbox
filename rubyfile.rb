e387fcd0529a96de80e71308a7729888afdeb7e4
Patrick Rawlings <admin@iPatty.local>
2013-02-04 15:54:33 -0500

    This a commit test with the -a command

--- rubyfile.rb
+++ rubyfile.rb
@@ -1,1 +1,6 @@
-# This is a ruby file.  I forgot to add it was created by Patty.
+# This is a ruby file.  I forgot to add it was created by Patty
+
+
+Also, don't give up son. 
+
+SCV good to go sir! 
