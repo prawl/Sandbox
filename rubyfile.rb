2251f5fbf137ca4d39c75e7274d07ae29850d2b3
Patrick Rawlings <prawlings@assetricity.com>
2013-02-25 13:51:03 -0500

    Testing Codeclimate

--- rubyfile.rb
+++ rubyfile.rb
@@ -1,6 +1,18 @@
-# This is a ruby file.  I forgot to add it was created by Patty
+<h1> Hello World</h1>
+<p>Greetings from the hello page.</p>
 
+<%= 1 + 1 %>
+ID =  <%= @id %> <br/>
+Next page: <%= @page + 1 %> <br/>
 
-Also, don't give up son. 
+<% target = "world" %>
 
-SCV good to go sir! 
+<br/>
+<br/><br/><br/>
+
+<%= "Hello #{target}" %>
+<% @array.each do |n| %> 
+<%= n %> <br />
+<% end %>
+
+<%= link_to("My first link", {:action => 'index'}) %>
